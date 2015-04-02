fe53702e0629d47d7f4b8965c19df373581a0fd6
f3z0 <gregory.james.ray@gmail.com>
2015-04-02 07:34:07 +0100

    f

--- app.js
+++ app.js
@@ -1,60 +1,68 @@
 var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-
-var routes = require('./routes/index');
-var users = require('./routes/users');
+ var mongo = require('mongodb');
 
 var app = express();
+var server = new mongo.Server('localhost', 27017, {auto_reconnect: true});
+db = new mongo.Db('test', server);
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+ app.get('/', function(req, res){
+  
+  var MongoClient = require('mongodb').MongoClient
+      , format = require('util').format;
+  MongoClient.connect('mongodb://127.0.0.1:27017/test', function (err, db) {
+      if (err) {
+          throw err;
+      } else {
+        db.collection('courses', function(err, collection) {
+                collection.distinct('school', function(err, items) {
+                  res.render('index.ejs', {items: items})
+                    db.close();
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+                });
+            });
+      }
+  });
+ });
 
-app.use('/', routes);
-app.use('/users', users);
+ app.get('/school/:school', function(req, res){
+    
+    var MongoClient = require('mongodb').MongoClient
+        , format = require('util').format;
+    MongoClient.connect('mongodb://127.0.0.1:27017/test', function (err, db) {
+        if (err) {
+            throw err;
+        } else {
+          db.collection('courses', function(err, collection) {
+            collection.find({school:req.params.school}, {'short-desc':1}).toArray(function(err, items) {
+              console.log(items)
+                    res.render('school.ejs', {school: req.params.school, items: items})
+                      db.close();
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
+                  });
+              });
+        }
     });
-  });
-}
+ });
 
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
+ app.get('/course/:id', function(req, res){
+    
+    var MongoClient = require('mongodb').MongoClient
+        , format = require('util').format;
+    MongoClient.connect('mongodb://127.0.0.1:27017/test', function (err, db) {
+        if (err) {
+            throw err;
+        } else {
+          db.collection('courses', function(err, collection) {
+            console.log(req.params.id)
+            collection.findOne( { _id: require('mongodb').ObjectID.createFromHexString(req.params.id) }, function(err, course) {
+              console.log(err)
+                    res.render('course.ejs', {course: course})
+                      db.close();
 
-
-module.exports = app;
+                  });
+              });
+        }
+    });
+ });
+ app.listen(8080);
+ console.log('Listening on port 8080...');
